2338ac1b2088ef77df24d573349510139cac2486
Tstark <samuelabiye00@gmail.com>
2025-10-15 07:55:47 +0100

    C++

---
+++ while_loop.cpp
@@ -0,0 +1,13 @@
+//The while loop loops through a block of code as long as a specified condition is true:
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 0;
+    while (i < 5){
+        cout << i << "\n";
+        i++;
+
+    }
+}
