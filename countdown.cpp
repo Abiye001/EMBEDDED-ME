e86a589363ef71f02ea829232aefbb0c961d60d1
Tstark <samuelabiye00@gmail.com>
2025-10-17 22:34:58 +0100

    C++

---
+++ countdown.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int countdown = 10;
+    while (countdown > 0){
+        cout << countdown << endl;
+
+        countdown--;
+    
+    }
+cout << "Happy New Year!!!!!!!!!!!!!!!! " << endl;
+}
---
+++ nested_loop.cpp
@@ -0,0 +1,16 @@
+/* Nested Loops
+It is also possible to place a loop inside another loop. This is called a nested loop.
+
+The "inner loop" will be executed one time for each iteration of the "outer loop":
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+}
